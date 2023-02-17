38c9b135595aa0b689465855a8f5cbcc00951b4d
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-02-18 02:37:58 +0900

    init

---
+++ src/test/kotlin/com/goofy/featureflag/GoofyFeatureFlagApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goofy.featureflag
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoofyFeatureFlagApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
